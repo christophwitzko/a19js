976a5ca12a4826356cb694eea7f11aaaa483f9a3
Christoph Witzko <github@christophwitzko.com>
2016-02-07 02:05:56 +0100

    feat(example): add server

--- example.js
+++ example.js
@@ -3,13 +3,15 @@ const crypto = require('crypto')
 const fs = require('fs')
 
 const Client = require('./Client')
+const Server = require('./Server')
 
+const server = new Server('./mydb', ['MYPASSWORD'], 8080)
 const hash = crypto.createHash('md5').update('MYPASSWORD').digest('hex')
 
 const client = new Client(`a19://${hash}@127.0.0.1:8080`)
 client.list((err, files) => {
   if (err) return console.log(err)
-  console.log(files)
+  console.log('FILES', files.length)
 })
 
 fs.createReadStream('test.txt').pipe(client.uploadStream(444, (err) => {
@@ -19,3 +21,8 @@ fs.createReadStream('test.txt').pipe(client.uploadStream(444, (err) => {
     console.log(err, data.toString())
   })
 }))
+
+client.download(444, (err, data) => {
+  if (err) return console.log(err)
+  console.log(err, data.toString())
+})
